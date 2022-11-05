db9b7d8e3919f58b30675a99824a2eb79081642e
Kevin-Illu <78170908+Kevin-Illu@users.noreply.github.com>
2022-11-05 15:23:16 -0600

    add navbar component

--- app/Components/NavBar.js
+++ app/Components/NavBar.js
@@ -11,15 +11,15 @@ class NavBar {
     //title fo current screen
     this.title_current;
     //trafic lights buttons
-    this._btn_minimize = new TrafficLight('#traffic-lights','minimize');
+    this._btn_minimize = new TrafficLight('#traffic-lights', 'minimize');
     this._btn_minimize.actionName = 'minimize';
     this._btn_minimize.icon = `<img src="./public/img/minimize.svg" alt="button close">`;
 
-    this._btn_maximize = new TrafficLight('#traffic-lights','maximize')
+    this._btn_maximize = new TrafficLight('#traffic-lights', 'maximize')
     this._btn_maximize.actionName = 'maximize';
     this._btn_maximize.icon = `<img src="./public/img/maximize.svg" alt="button close">`;
 
-    this._btn_close = new TrafficLight('#traffic-lights','close')
+    this._btn_close = new TrafficLight('#traffic-lights', 'close')
     this._btn_close.actionName = 'close';
     this._btn_close.icon = `<img src="./public/img/close.svg" alt="button close">`;
   }
@@ -42,7 +42,7 @@ class NavBar {
 
   //TODO: i need to implement this UnU
   setCurrent = (current) => {
-    // console.log(current);
+    console.log(current);
   }
 }
 
